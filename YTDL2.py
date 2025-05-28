8345e25bb3e931f1dcd73012b47f939eab627b8b
minhung1126 <minhung1126@gmail.com>
2025-05-28 08:32:43 +0800

    新增影片下載功能，修正檔案讀取編碼，並優化下載流程

--- YTDL2.py
+++ YTDL2.py
@@ -1,6 +1,10 @@
+import os
 import json
 import subprocess
+import re
+from urllib.parse import urlparse, parse_qs
 
+META_DIR = os.path.join(os.getcwd(), 'meta')
 
 class Video:
     def __init__(self, meta_filepath: str):
@@ -15,7 +19,7 @@ class Video:
 
     def _read_meta(self):
         try:
-            with open(self.meta_filepath, 'r') as f:
+            with open(self.meta_filepath, 'r', encoding='utf-8') as f:
                 meta = json.loads(f.read())
             return meta
         except FileNotFoundError:
@@ -32,9 +36,9 @@ class Video:
 
     def download(self):
         args = [
-            'yt-dlp', '-F',
+            'yt-dlp', '-f', 'bv+ba'
             '-S', 'res,hdr,+codec:vp9.2:opus,+codec:vp9:opus,+codec:vp09:opus,+codec:avc1:m4a,+codec:av01:opus,vbr',
-            self.url,
+            '--load-info-json', self.meta_filepath,
         ]
         subprocess.run(
             args
@@ -46,12 +50,49 @@ class Playlist:
 
 
 def download_from_url(url: str):
-    ...
+    args = [
+        'yt-dlp',
+        '--no-download',
+        '--no-write-playlist-metafiles',
+        '-o', os.path.join(META_DIR, f"%(title)s.%(id)s.info.json"),
+        '--write-info-json',
+        url
+    ]
+    parsed = urlparse(url)
+    query = parse_qs(parsed.query)
+    if '/playlist' in parsed.path:  # is desired to download as a playlist
+        pass
+    else:
+        args.append('--no-playlist')
+
+    subprocess.run(args)
+
+
+def load_videos() -> list[Video]:
+    videos = (
+        Video(os.path.join(META_DIR, filename))
+        for filename in os.listdir(META_DIR)
+    )
+
+    return videos
+
+
+def parse_user_action():
+    if os.path.isdir(META_DIR):
+        # Ask if resume download
+        ...
+
+    resp = input("URL: ")
+    download_from_url(resp)
 
 
 def main():
-    Video("https://www.youtube.com/watch?v=YTO65C2Brg4").download()
-    Video("https://www.youtube.com/watch?v=2n0OS1R0JFc").download()
+    parse_user_action()
+    videos = load_videos()
+    for vid in videos:
+        vid.download()
+    # Video("https://www.youtube.com/watch?v=YTO65C2Brg4").download()
+    # Video("https://www.youtube.com/watch?v=2n0OS1R0JFc").download()
 
 
 if __name__ == "__main__":
